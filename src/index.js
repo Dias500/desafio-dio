ad39508db79be84fce515e1990a55db5790d285e
Dias500 <alan.dias120@outlook.com>
2025-03-28 13:22:33 -0300

    alteração do arquivo index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!")
+
+let contador1 = 0
+
+for(contador = 0; contador <=10; contador++){
+    console.log("Contador: " + contador)
+}
+
+while (contador1 <= 6) {
+    console.log("Contador: " + contador1)
+    contador1++
+}
